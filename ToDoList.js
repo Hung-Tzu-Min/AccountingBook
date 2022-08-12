253d36f30a98e3d4dbcc3393f5b0a2289a73406e
DESKTOP-3VGANNM\vince <vince666asd@gmail.com>
2022-08-12 20:35:28 +0800

    init

---
+++ ToDoList.js
@@ -0,0 +1,5 @@
+
+var addition = document.getElementById("addition");
+var date = document.getElementById("date");
+var documental = document.getElementById("documental");
+var view3 = document.getElementById("view3");
